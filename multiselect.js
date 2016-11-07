c541a2d80e8d988cec007f30eec70eb24d7638d9
Patrick Werner <patrick@freisicht.net>
2016-11-07 19:02:08 +0100

    Noch eine kleine Überarbeitung

--- multiselect.js
+++ multiselect.js
@@ -1,19 +1,69 @@
 var multiSelect = (function(selector, options) {
     "use strict";
 
+    // Gibt es keinen Selector, gibt es kein Plugin.
+    if (!checkType(selector, 'String') || !selector.trim())
+        return;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // EINSTELLUNGEN ------------------------------------------------------------------------------------------------ //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    var settings = {
+        searchBar: false,
+        useOptGroups: true,
+        ofString: 'of',
+        cancelString: 'CANCEL',
+        saveString: 'SAVE',
+
+        // Hooks
+        preInit: null,
+        afterInit: null,
+        onChange: null,
+        onSave: null,
+    };
+
+    // Die übergebenen Optionen überschreiben die Standard-Settings
+    if (typeof options !== 'undefined')
+        for (var property in options)
+            if (options.hasOwnProperty(property))
+                settings[property] = options[property];
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // NÜTZLCIHE FUNKTIONEN ----------------------------------------------------------------------------------------- //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Eigener Exception-Type
     function MultiSelectException(message) {
         this.message = message;
         this.name    = 'MultiSelectException';
     }
 
-    function checkType(value, type) {
-
-        return Object.prototype.toString.call(value) === '[object ' + type + ']';
+    // Holt sich den Typen aus Sicht der toString()-Methode der Objekte in JavaScript
+    function getType(value) {
+        return Object.prototype.toString.call(value).replace('[object ', '').replace(']', '');
     }
+
+    function checkType(value, type) {
+        return getType(value) === type;
+    }
+
+    // Wartet bis der Inhalt der Seite geladen ist bevor der Code ausgeführt wird
+    function ready(fn) {
+        if (document.readyState != 'loading') {
+            fn();
+        } else {
+            document.addEventListener('DOMContentLoaded', fn);
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // DYNAMISCHE FUNKTIONEN (GETTER, SETTER, ADDER) ---------------------------------------------------------------- //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     var get = function(what) {
         if (typeof this[what] === 'undefined')
-            throw new MultiSelectException('get: param 0: no defined value to set');
+            throw new MultiSelectException('get: param 0: no defined value to get');
 
         switch (true) {
             // Objekt
@@ -32,8 +82,15 @@ var multiSelect = (function(selector, options) {
     }
 
     var set = function(what, value) {
+        if (typeof this[what] === 'undefined')
+            throw new MultiSelectException('get: param 0: no defined value to set');
+
+        var inType = true;
+
+        for (var i in this[what].type)
+
         if (!checkType(value, this[what].type))
-            throw new MultiSelectException('set: param 1 not typeof ' + this[what].type);
+            throw new MultiSelectException('set: param 1 not typeof ' + this[what].type + ' (is ' + getType(value) + ')');
 
         if (typeof what === 'undefined')
             throw new MultiSelectException('set: param 0: no defined value to set');
@@ -42,6 +99,9 @@ var multiSelect = (function(selector, options) {
     };
 
     var add = function(what, value) {
+        if (typeof this[what] === 'undefined')
+            throw new MultiSelectException('add: param 0: no defined value to add something');
+
         if (!checkType(this[what].value, 'Array'))
             throw new MultiSelectException('add: can\'t push value into a non-array');
 
@@ -54,8 +114,12 @@ var multiSelect = (function(selector, options) {
         this[what].value.push(value);
     };
 
-    var Option = (function() {
-        function Option(element) {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // KLASSEN ------------------------------------------------------------------------------------------------------ //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    var msOption = (function() {
+        function msOption(element) {
             this.className = 'Option';
 
             // Private Variables
@@ -65,7 +129,7 @@ var multiSelect = (function(selector, options) {
                 disabled: { value: false, type: 'Boolean' },
                 isOptionGroup: { value: false, type: 'Boolean' },
                 options: { value: [], type: 'Object', className: 'Option' },
-                domElement: { value: null, type: 'HTMLBodyElement' },
+                domElement: { value: null, type: 'HTMLOptGroupElement|HTMLOptGroupElement' },
             };
 
             // Dynamic Values
@@ -78,7 +142,7 @@ var multiSelect = (function(selector, options) {
                 this.set('domElement', element);
         };
 
-        return Option;
+        return msOption;
     })();
 
     var Dropdown = (function() {
@@ -133,6 +197,7 @@ var multiSelect = (function(selector, options) {
 
             // Private Variables
             var _private = {
+                domElement: { value: null, type: 'HTMLSelectElement' },
                 options: { value: [], type: 'Object', className: 'Option' },
                 dropdown: { value: null, type: 'Object', className: 'Dropdown' },
                 togglebutton: { value: null, type: 'Object', className: 'ToggleButton' },
@@ -147,13 +212,36 @@ var multiSelect = (function(selector, options) {
             // construct param
             if (typeof element !== 'undefined')
                 this.set('domElement', element);
+
+            this.init();
+        };
+
+        UIController.prototype.init = function() {
+            var that = this;
+
+            var select  = this.get('domElement');
+            var options = select.querySelectorAll('optgroup, option');
+
+            var currentOptionGroup = null;
+
+            Array.prototype.forEach.call(options, function(el, i) {
+                if (!settings.useOptGroups && el.tagName === 'OPTGROUP')
+                    return;
+
+                var option = new msOption(el);
+
+                if (option.get('isOptionGroup'))
+                    currentOptionGroup = option;
+                else
+                    currentOptionGroup.add('options', option);
+            });
         };
 
         return UIController;
     })();
 
     var MultiSelect = (function() {
-        function MultiSelect(selector) {
+        function MultiSelect(settings) {
             this.className = 'MultiSelect';
 
             // Private Variables
@@ -167,35 +255,36 @@ var multiSelect = (function(selector, options) {
             this.add = add.bind(_private);
             this.get = get.bind(_private);
 
-            // construct param
-            
+            if (typeof settings !== 'undefined')
+                this.set('settings', settings);
+        };
+
+        MultiSelect.prototype.init = function(selector) {
+            var that = this;
+
+            var selects = document.querySelectorAll(selector);
+
+            if (!selects.length)
+                throw new MultiSelectException('init: No Selects could be found');
+
+            Array.prototype.forEach.call(selects, function(select, i){
+                var uiController = new UIController(select);
+                that.add('uiControllers', uiController);
+            });
         };
 
         return MultiSelect;
     })();
 
-    if (!checkType(selector, 'String') || !selector.trim())
-        return;
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // ANWENDUNG ---------------------------------------------------------------------------------------------------- //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    var settings = {
-        searchBar: false,
-        useOptGroups: true,
-        ofString: 'of',
-        cancelString: 'CANCEL',
-        saveString: 'SAVE',
+    var multiSelect = new MultiSelect(settings);
 
-        // Hooks
+    ready(function() {
+        multiSelect.init(selector);
+    });
 
-        preInit: null,
-        afterInit: null,
-        onChange: null,
-        onSave: null,
-    };
-
-    if (checkType(options, 'Object'))
-        for (var property in options)
-            if (options.hasOwnProperty(property))
-                settings[property] = options[property];
-
-    // var elements = Array.prototype.slice.call(document.querySelectorAll(selector));
+    return multiSelect;
 });
